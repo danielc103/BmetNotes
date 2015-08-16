6a6eb09cb7d5d96f1811642e774f00e2b70f0c24
Daniel Carroll <danielc103@gmail.com>
2015-08-16 15:43:18 -0400

    trying to resolve theme errors

--- app/src/main/java/com/android/bmetnotes/MainActivity.java
+++ app/src/main/java/com/android/bmetnotes/MainActivity.java
@@ -4,13 +4,14 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationDrawerCallbacks {
 
     /**
