417036c73726edc0d21f34726bf9f5d2bfed72dc
Daniel Carroll <danielc103@gmail.com>
2015-08-18 20:35:56 -0400

    added fragments - need code behind

---
+++ app/src/main/java/com/android/bmetnotes/Note/NoteFragment.java
@@ -0,0 +1,9 @@
+package com.android.bmetnotes.Note;
+
+import android.app.Fragment;
+
+/**
+ * Created by Daniel Carroll on 8/18/2015.
+ */
+public class NoteFragment extends Fragment {
+}
---
+++ app/src/main/java/com/android/bmetnotes/Project/ProjectFragment.java
@@ -0,0 +1,9 @@
+package com.android.bmetnotes.Project;
+
+import android.app.Fragment;
+
+/**
+ * Created by Daniel Carroll on 8/18/2015.
+ */
+public class ProjectFragment extends Fragment {
+}
