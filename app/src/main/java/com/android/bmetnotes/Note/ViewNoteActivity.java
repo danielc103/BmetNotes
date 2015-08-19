5f6cdfd20decba8d626d8ee4c5be5d23b6fd24a7
Daniel Carroll <danielc103@gmail.com>
2015-08-18 20:37:35 -0400

    added activity to view note once card is clicked (need code)

---
+++ app/src/main/java/com/android/bmetnotes/Note/ViewNoteActivity.java
@@ -0,0 +1,9 @@
+package com.android.bmetnotes.Note;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Daniel Carroll on 8/18/2015.
+ */
+public class ViewNoteActivity extends ActionBarActivity {
+}
