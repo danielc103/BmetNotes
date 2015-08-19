6c1249b6a66f08373bbf714931f89c8b6f03c4ec
Daniel Carroll <danielc103@gmail.com>
2015-08-18 20:43:12 -0400

    view notes in project activity (need code)

---
+++ app/src/main/java/com/android/bmetnotes/Project/ViewProjectNotesActivity.java
@@ -0,0 +1,10 @@
+package com.android.bmetnotes.Project;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Daniel Carroll on 8/18/2015.
+ */
+public class ViewProjectNotesActivity extends AppCompatActivity {
+}
